0d4655ebb8783335af190d5502d21adc404b2b59
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-08 15:14:56 +0200

    add background images and colors depending on terrain class

--- map-gen/functions.php
+++ map-gen/functions.php
@@ -7,38 +7,26 @@ function roll_1d(int $number): int
 
 function terrainType(): string
 {
-    $score = roll_1d(10);
+    $score = roll_1d(8);
 
     switch ($score) {
         case 1:
-            return "canyon";
-            break;
-        case 2:
             return "hill";
-            break;
-        case 3:
+        case 2:
             return "desert";
-            break;
-        case 4:
+        case 3:
             return "forest";
-            break;
-        case 5:
+        case 4:
             return "lake";
-            break;
-        case 6:
+        case 5:
             return "swamp";
-            break;
-        case 7:
+        case 6:
             return "mountain";
-            break;
-        case 8:
+        case 7:
             return "plain";
-            break;
-        case 9:
-            return "river";
-            break;
-        case 10:
+        case 8:
             return "volcano";
-            break;
+        default :
+            return "";
     }
 }
