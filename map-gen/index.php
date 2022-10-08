4b49c455e8a7a89d2e486c7eee2f1570fbb3a1f5
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-08 11:42:46 +0200

    make base grid structure

---
+++ map-gen/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>RPG | Map Generator</title>
+</head>
+<body>
+<h1>Random Map Generator</h1>
+<table>
+    <?php
+    $gridSize = 10;
+    for ($x = 0; $x < $gridSize; $x++) {
+        echo "<tr>";
+        for ($y = 0; $y < $gridSize; $y++) {
+            echo "<td></td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+</table>
+</body>
+</html>
