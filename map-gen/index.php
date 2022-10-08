db26c7c378e13886a9d12eaf10d8afcbb54049a4
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-08 18:05:30 +0200

    set html form for map parameters selection by user

--- map-gen/index.php
+++ map-gen/index.php
@@ -9,20 +9,52 @@
     <title>RPG | Map Generator</title>
 </head>
 <body>
-<?php include "functions.php" ?>
 <h1>Random Map Generator</h1>
-<table>
-    <?php
-    $gridSize = 10;
-    for ($x = 0; $x < $gridSize; $x++) {
-        echo "<tr>";
-        for ($y = 0; $y < $gridSize; $y++) {
-            echo "<td class=" . terrainType() . "></td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-    ?>
-</table>
+<div class="form-container">
+    <form action="map.php" method="post">
+        <h2>Parameters:</h2>
+        <p>How much do you want of each terrain type? (in %)</p>
+        <div>
+            <label for="plain">Plains:</label>
+            <input type="number" id="plain" name="plain">
+        </div>
+        <div>
+            <label for="hill">Hills:</label>
+            <input type="number" id="hill" name="hill">
+        </div>
+        <div>
+            <label for="forest">Forests:</label>
+            <input type="number" id="forest" name="forest">
+        </div>
+        <div>
+            <label for="mountain">Mountains:</label>
+            <input type="number" id="mountain" name="mountain">
+        </div>
+        <div>
+            <label for="lake">Lakes:</label>
+            <input type="number" id="lake" name="lake">
+        </div>
+        <div>
+            <label for="swamp">Swamps:</label>
+            <input type="number" id="swamp" name="swamp">
+        </div>
+        <div>
+            <label for="desert">Deserts:</label>
+            <input type="number" id="desert" name="desert">
+        </div>
+        <div>
+            <label for="volcano">Volcanoes:</label>
+            <input type="number" id="volcano" name="volcano">
+        </div>
+        <p>What size is your map? (in squares)</p>
+        <div>
+            <label for="gridsize">Grid Size:</label>
+            <input type="number" id="gridesize" name="gridsize">
+        </div>
+        <div class="btn-container">
+            <input type="submit" id="submit" value="Submit">
+        </div>
+    </form>
+</div>
 </body>
 </html>
---
+++ map-gen/map.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>RPG | Map Generator</title>
+</head>
+<body>
+
+<?php
+
+// set a gridsize
+$gridSize = $_POST["gridsize"];
+$squareNumber = $gridSize*$gridSize;
+
+$terrainArray = [];
+
+// set terrain proportion/quantity in percentage (%)
+$qtOfHill = ceil($_POST["hill"] * $squareNumber /100);
+$qtOfDesert = ceil($_POST["desert"] * $squareNumber /100);
+$qtOfForest = ceil($_POST["forest"] * $squareNumber /100);
+$qtOfLake = ceil($_POST["lake"] * $squareNumber /100);
+$qtOfSwamp = ceil($_POST["swamp"] * $squareNumber /100);
+$qtOfMountain = ceil($_POST["mountain"] * $squareNumber /100);
+$qtOfPlain = ceil($_POST["plain"] * $squareNumber /100);
+$qtOfVolcano = ceil($_POST["volcano"] * $squareNumber /100);
+
+// stock all the terrain type in a big array
+for ($i = 0; $i < $qtOfHill; $i++) {
+    array_push($terrainArray, "hill");
+}
+for ($i = 0; $i < $qtOfDesert; $i++) {
+    array_push($terrainArray, "desert");
+}
+for ($i = 0; $i < $qtOfForest; $i++) {
+    array_push($terrainArray, "forest");
+}
+for ($i = 0; $i < $qtOfLake; $i++) {
+    array_push($terrainArray, "lake");
+}
+for ($i = 0; $i < $qtOfSwamp; $i++) {
+    array_push($terrainArray, "swamp");
+}
+for ($i = 0; $i < $qtOfMountain; $i++) {
+    array_push($terrainArray, "mountain");
+}
+for ($i = 0; $i < $qtOfPlain; $i++) {
+    array_push($terrainArray, "plain");
+}
+for ($i = 0; $i < $qtOfVolcano; $i++) {
+    array_push($terrainArray, "volcano");
+}
+
+// shuffle randomly the array
+shuffle($terrainArray);
+
+// divide the big array (100 elements) into a 2D array (10x10 elements)
+$terrainType = [];
+for ($i = 0; $i < $gridSize; $i++) {
+    $terrainType[$i] = array_splice($terrainArray, 0, $gridSize);
+}
+
+?>
+
+<h1>Random Map Generator</h1>
+
+<table>
+    <?php
+    for ($x = 0; $x < $gridSize; $x++) {
+        echo "<tr>";
+        for ($y = 0; $y < $gridSize; $y++) {
+            echo "<td class=" . $terrainType[$x][$y] . "></td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+</table>
+
+</body>
+</html>
+
