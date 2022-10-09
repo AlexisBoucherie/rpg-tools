b98720b16832653241ff5e6f5a3de67bc7b7ce3d
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-09 14:55:14 +0200

    start array database

---
+++ sidequest-gen/db-sidequest-gen.php
@@ -0,0 +1,130 @@
+<?php
+
+$characterClass = [
+    "barbarian",
+    "bard",
+    "cleric",
+    "druid",
+    "sorcerer",
+    "warrior",
+    "wizard",
+    "monk",
+    "paladin",
+    "ranger",
+    "rogue"
+];
+
+$characterRace = [
+    "elf",
+    "halfling",
+    "human",
+    "dwarf",
+    "half-elf",
+    "half-orc",
+    "dragonborn",
+    "gnome",
+    "tieffelin"
+];
+
+$characterInNeed = [
+    "craftsman",
+    "artist",
+    "merchant",
+    "nobleman",
+    "farmer",
+    "member-of-the-clergy",
+    "soldier",
+    "hero"];
+
+$whatIsTheNeed = [
+    "escort",
+    "delivery",
+    "materials",
+    "message",
+    "protection",
+    "research",
+    "personal-problem",
+    "strong-arm-service"
+];
+
+$personalProblem = [
+    "agression",
+    "scam",
+    "creature-atk",
+    "calumny",
+    "blackmail",
+    "debt",
+    "missing",
+    "duel",
+    "imprisonment",
+    "kidnapping",
+    "harassment",
+    "humiliation",
+    "intimidation",
+    "sick",
+    "malediction",
+    "murder",
+    "negotiation",
+    "weird-events",
+    "visions",
+    "theft"
+];
+
+$strongArmService = [
+    "capture",
+    "spy",
+    "blackmail",
+    "intimidate",
+    "kill",
+    "rob"
+];
+
+$antagonist = [
+    "craftsman",
+    "artist",
+    "bandit",
+    "cultist",
+    "guild",
+    "merchant",
+    "mercenary",
+    "nobleman",
+    "member-of-the-clergy",
+    "commoner",
+    "soldier",
+    "hero"
+];
+
+$dungeon = [
+    "crypt",
+    "temple",
+    "catacombs",
+    "shrine",
+    "castle",
+    "tower",
+    "sewer",
+    "cavern",
+    "mine"
+];
+
+$occupiedBy = [
+    "aberrations",
+    "bandits",
+    "beasts",
+    "drows",
+    "duergars",
+    "elementals",
+    "elves",
+    "fanatics",
+    "ghosts",
+    "faeries",
+    "fiends",
+    "giants",
+    "gobelins",
+    "humans",
+    "kobolds",
+    "undead",
+    "dwarves",
+    "orcs",
+    "renegades",
+    "vampires"
+];
---
+++ sidequest-gen/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RPG | Side Quest Generator</title>
+</head>
+<body>
+    <?php require_once "db-sidequest-gen.php" ?>
+    <h1>Side Quest Generator</h1>
+
+</body>
+</html>
