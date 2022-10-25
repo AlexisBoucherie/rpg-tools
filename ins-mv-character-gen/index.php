3a8248aa006f7d05fb152b2b61e05feb06d3a1d6
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-10-25 14:35:12 +0200

    create todolist

---
+++ ins-mv-character-gen/index.php
@@ -0,0 +1,73 @@
+<!--
+    DATABASE avec
+    > PRINCES-DEMONS
+    > POUVOIRS (et POUVOIRS SPE)
+    > POUVOIRS DE GRADE
+    > LIMITATIONS
+    > ARMES / ARMURES
+    et peut-être :
+    > VEHICULES
+    > SERVITEURS
+
+    FONCTIONS PHP pour
+    (suivant profil, en random - filou/martial/social...)
+    > générer les caracs
+    > générer des talents types
+    si pouvoir arme, vehicule ou serviteur tiré
+    > générer l'item en question
+
+    JAVASCRIPT pour
+    > voir les limites associées aux talents, en hover
+-->
+
+<!doctype html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>INS character generator</title>
+</head>
+<body>
+
+<article>
+    <section>
+        <div class="one-third">
+            <img src="" alt="logo-ins">
+            <div class="caracs"><!--FO-VO-PR-AG-PE-AP--></div>
+            <div class="pv-pp"><!--PV max/current, PP max/current--></div>
+        </div>
+        <img src="" alt="Character picture" class="one-third">
+        <div class="one-third">
+            <div class="identity"><!--NOM GRADE SUPERIEUR--></div>
+            <div class="cover"><!--NOM JOB ADRESSE--></div>
+            <div class="description"><!--AGE TAILLE POIDS--></div>
+        </div>
+    </section>
+    <section class="tum">
+        <!-- TABLEAU TUM -->
+    </section>
+    <section>
+        <!--DOUBLE DIV talents (physique, combat, comm, ruse, artistique, scientifique)
+         + hierarchie (invoc, pouvoirs de grade, recup pp, limitations -->
+    </section>
+    <section>
+        <!--ARMES (contact et CaC) -->
+    </section>
+    <section>
+        <!--POUVOIRS-->
+    </section>
+    <section>
+        <!--NOTES ET EQUIPEMENTS (vue du corps avec différents équipement, façon rpg + sac à dos ?)-->
+    </section>
+    <section>
+        <!--VEHICULES-->
+    </section>
+    <section>
+        <!--SERVITEURS-->
+    </section>
+</article>
+
+</body>
+</html>
